2e2d08732487ae5eb0e69eaa020022d44f5f041c
silverwind <charlieqiaoperth@gmail.com>
2019-06-09 22:43:07 +0800

    auth token adminuser all done

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,19 @@ const customerRoute = require('./routes/customer');
 // const businessRoute = require('./routes/business');
 // const bookingRoute = require('./routes/booking');
 // const categoryRoute = require('./routes/category');
-// const userRoute = require('./routes/user');
-// const authRoute = require('./routes/auth');
-// const authGuard = require('./middleware/authGuard');
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+const authGuard = require('./middlewares/authGuard');
 
 const router = express.Router();
 
-router.use('/customer', customerRoute);
+router.use('/customer', authGuard, customerRoute);
+
 // router.use('/business', businessRoute);
 // router.use('/booking', bookingRoute);
 // router.use('/category', categoryRoute);
-// router.use('/users', userRoute);
-// router.use('/auth', authRoute);
+router.use('/users', userRoute);
+router.use('/auth', authRoute);
 router.get('/',  (req,res) => res.status('200').json('Welcome to the handyman-cms api! Visit /api-docs for help'))
 
 module.exports = router;
