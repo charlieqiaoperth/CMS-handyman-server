babcc12be97286ca30818f69800b360aa24fdca3
silverwind <charlieqiaoperth@gmail.com>
2019-06-14 10:08:15 +0800

    fixbug about authincontroller

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { loginUser } = require('../controllers/auth');
+const { loginUser } = require('../controllers/Auth');
 const router = express.Router();
 
 router.post('/', loginUser);
