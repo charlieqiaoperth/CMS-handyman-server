10457cc7f597b30bad932b0ae2b91f1174dac816
charlie qiao <charlieqiaoperth@gmail.com>
2019-08-21 20:31:50 +0800

    remove the authGuard middleware

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,15 @@ const orderRoute = require('./routes/order');
 const categoryRoute = require('./routes/category');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
-const authGuard = require('./middlewares/authGuard');
+// const authGuard = require('./middlewares/authGuard');
 
 const router = express.Router();
 
 // router.use('/customer', authGuard, customerRoute);
-router.use('/customers', authGuard, customerRoute);
-router.use('/businesses', authGuard, businessRoute);
-router.use('/orders', authGuard, orderRoute);
-router.use('/categories', authGuard, categoryRoute);
+router.use('/customers', customerRoute);
+router.use('/businesses', businessRoute);
+router.use('/orders', orderRoute);
+router.use('/categories', categoryRoute);
 router.use('/users', userRoute);
 router.use('/auth', authRoute);
 router.get('/',  (req,res) => res.status('200').json('Welcome to the handyman-cms api! Visit /api-docs for help'))
